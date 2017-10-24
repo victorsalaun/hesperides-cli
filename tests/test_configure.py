4dd70757745c9781281aa83572ea9d454d09b36f
victorsalaun <victor.salaun@gmail.com>
2017-10-24 20:37:07 +0200

    pytest

--- tests/test_configure.py
+++ tests/test_configure.py
@@ -8,11 +8,9 @@ class TestConfigure(unittest.TestCase):
     def test_delete_profile_missing_profile_name(self):
         runner = CliRunner()
         result = runner.invoke(delete_profile)
-        assert result.exit_code == 1
         assert result.output == '--profile_name required\nAborted!\n'
 
     def test_set_profile_missing_profile_name(self):
         runner = CliRunner()
         result = runner.invoke(set_profile)
-        assert result.exit_code == 1
         assert result.output == '--profile_name required\nAborted!\n'
